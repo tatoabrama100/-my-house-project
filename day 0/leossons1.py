44e0fa590d91903cce79dcea9796cd8946e34a9b
tatoabrama100 <tatoabrama9@gmail.com>
2024-01-25 19:58:05 +0400

    first project

---
+++ day 0/leossons1.py
@@ -0,0 +1,101 @@
+from turtle import *
+
+# we want to hous #vashenebt saxls
+ 
+#step one  square # jer gvchirdeba kvadrati
+speed(30)
+# shape("turtle") es aris   "ku"
+begin_fill()
+width(5)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+ # end of square #) dasrulda kvadrati
+
+
+# browing is door ) vaketebt karebs
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+
+forward(120)
+right(90)
+
+forward(60)
+right(90)
+forward(120)
+end_fill()
+# off the door ) dasruleba
+
+penup()  #aviget kalami ))
+goto(200 , 200) # gaushvit )
+pendown()  #  davadet isev saxatavad ))
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+# dasruleba ))
+ # browind is the window0   #vaketebt fajaras)
+
+begin_fill()
+color("purple")
+left(30)
+forward(40)     #left window)    #(marcxena fanjara )
+color("blue")
+left(90)
+forward(50)
+right(90)
+forward(40)
+right(90)
+forward(50)
+right(180)
+forward(25)
+left(90)
+forward(40)
+end_fill()
+
+    # right windown
+
+color("purple")
+penup()
+goto(200,200)              # right window) # marjvena fanjara )
+pendown()
+
+
+begin_fill()
+right(180)
+forward(40)
+color("blue")
+right(90)
+forward(50)
+left(90)
+forward(40)
+left(90)
+forward(50)
+left(180)
+forward(25)
+right(90)
+forward(40)
+end_fill()
+
+
+
+
+exitonclick()
+
